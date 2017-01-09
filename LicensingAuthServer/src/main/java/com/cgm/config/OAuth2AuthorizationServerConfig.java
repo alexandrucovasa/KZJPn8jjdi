e5fc118f3ca82102bcee580a70a37f19a5bc5cce
andrei.urse <andrei.urse1@gmail.com>
2017-01-09 15:10:30 +0100

    Trigger build

--- LicensingAuthServer/src/main/java/com/cgm/config/OAuth2AuthorizationServerConfig.java
+++ LicensingAuthServer/src/main/java/com/cgm/config/OAuth2AuthorizationServerConfig.java
@@ -66,7 +66,7 @@ public class OAuth2AuthorizationServerConfig implements AuthorizationServerConfi
 
                 .authorizationCodeServices(new JdbcAuthorizationCodeServices(dataSource))
                 //// TODO: 12/7/2016 Create new approval store
-                //test
+                //test2
                 .approvalStore(new JdbcApprovalStore(dataSource));
     }
 
